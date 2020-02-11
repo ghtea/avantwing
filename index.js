613b607657368fdd6647b0a76b50c72bdf51510e
PJH <eiirwp@gmail.com>
2020-02-11 20:28:28 +0900

    w

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ window.onload = function(){
                     render(){
                         return React.createElement('h1', null, 'Greetings, ' + this.props.name + '!');
                     }
-          }
+          };
           
-          const App = () => {
-                    return React.createElement('div', null, 'Todo App을 만들자!');
-          }
+          class App extends React.Component{
+                    render(){
+                              return React.createElement('div', null, 'Todo App을 만들자!');
+                    }
+          };
           
           ReactDOM.render(React.createElement(Greetings, { name : 'Chris' }),
                     document.getElementById('root')
           );
           ReacrDOM.render(React.createElement(App),
-                    document.getElementById('root2')
+                    document.getElementById('root')
           );
 };
         
