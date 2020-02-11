a82fbf8c7a83fe17938ab6ab919abc077c98cc2d
PJH <eiirwp@gmail.com>
2020-02-11 19:54:28 +0900

    q

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ window.onload = function(){
                         return React.createElement('h1', null, 'Greetings, ' + this.props.name + '!');
                     }
           }
+          
+          const App = () => {
+                    return <div> Todo App을 만들자! </div>;
+          }
+          
           ReactDOM.render(React.createElement(Greetings, { name : 'Chris' }),
+                    document.getElementById('root')
+          );
+          ReacrDOM.render(React.createElement(App),
                     document.getElementById('root')
           );
 };
