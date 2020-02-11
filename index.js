4e540ad30b0e5988146817ff219e63cafbbc15c9
PJH <eiirwp@gmail.com>
2020-02-11 19:31:17 +0900

    q

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+          class Greetings extends React.Component{
+                    render(){
+                        return React.createElement('h1', null, 'Greetings, ' + this.props.name + '!');
+                    }
+          }
+          ReactDOM.render(React.createElement(Greetings, { name : 'Chris' }),
+          document.getElementById('root')
+          );
+};
+        
