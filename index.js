a5b5b51e6ec5bd66bb76a1b418e17f7ab363a833
PJH <eiirwp@gmail.com>
2020-02-13 20:10:52 +0900

    q

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ window.onload = function(){
                     }
           };
           
+          const TodoTemplate = ({children}) => {
+                    return (
+                              <div className="TodoTemplate">
+                                   <div className = "app-title">일정 관리</div>
+                                   <div className = "content">{children}</div>
+                              </div>
+                    );
+          };
+          
+          }
+          
+          
           const App = () => {
-                    return React.createElement('div', null, 'Todo App을 만들자!');
+                    return React.createElement('TodoTemplate', null, 'Todo App을 만들자!');
           };
           
           ReactDOM.render(React.createElement(Greetings, { name : 'Chris' }),
